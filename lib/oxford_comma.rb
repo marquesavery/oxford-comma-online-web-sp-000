fb092c16674b86a34e8d041db5bb0e2adef8c605
Marques Avery <averymarques4@gmail.com>
2020-07-31 18:07:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array[2]
+    array[-1].prepend("and ")
+    array.join(", ")
+  elsif array[1]
+    array[-1].prepend("and ")
+    array.join(" ")
+  else
+    array.join
+  end
 end
